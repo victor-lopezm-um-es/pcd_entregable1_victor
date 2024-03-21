06303d9008f8c1f4c93da55c89b48cd8800c7f20
victor-lopezm-um-es <victor.lopezm@um.es>
2024-03-21 19:04:52 +0100

    Añado pruebas unitarias para eliminar estudiante y eliminar miembro de departamento

--- sis_uni_test.py
+++ sis_uni_test.py
@@ -13,6 +13,22 @@ def test_añadir_miembroDepartamento():
     uni.añadir_miembro_departamento(miembro)
     assert len(uni._miembros_departamento) == 1
 
+def test_eliminar_estudiante():
+    uni = Universidad(estudiantes=[], miembros_departamento=[])
+    estudiante = Estudiante("Roberto", "12345678A", "Calle Echegaray, 27", Sexo.VARON, ["PCD", "IE"])
+    uni.añadir_estudiante(estudiante)
+    assert len(uni._estudiantes) == 1
+    uni.eliminar_estudiante("Roberto")
+    assert len(uni._estudiantes) == 0
+
+def test_eliminar_miembroDepartamento():
+    uni = Universidad(estudiantes=[], miembros_departamento=[])
+    miembro = ProfesorAsociado("Marisa", "87654321B", "Calle San Antonio", Sexo.MUJER, Departamento.DIIC, ["AMD"])
+    uni.añadir_miembro_departamento(miembro)
+    assert len(uni._miembros_departamento) == 1
+    uni.eliminar_miembro_departamento("Marisa")
+    assert len(uni._miembros_departamento) == 0
+
 def test_cambiar_Departamento():
     uni = Universidad(estudiantes=[], miembros_departamento=[])
     p1 = ProfesorTitular(nombre='Josefa', dni='12312344E', direccion='C/ Calasparra, 23', 
